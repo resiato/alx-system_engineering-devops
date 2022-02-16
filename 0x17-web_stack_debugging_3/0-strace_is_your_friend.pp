18f535943105f7a2c0f03f188dcddbc5addbd6b9
resiato <resiatolaitete@gmail.com>
2022-02-16 16:32:36 +0000

    Initial commit

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Code to fix a 500 error returned by Apache
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+The aim of this project is to find out what is making a container running Wordpress on Apache2 respond with a 500 status code.
